a6f233334c66aa099b165b63f7a5c73402a00cc9
Iago martins <martinsiagosaraiva@gmail.com>
2023-10-23 17:56:45 -0300

    reward card

--- src/app/components/RewardComponent/index.tsx
+++ src/app/components/RewardComponent/index.tsx
@@ -5,8 +5,8 @@ export const RewardComponent: React.FC<RewardComponentProps> = ({ reward }) => {
   console.log('reward', reward);
   return (
     <div className='flex flex-col w-5/12 h-[25vh] shadow-md'>
-      <div className='h-4/5 relative '>
-        <div className='h-4/5 relative p-3 '>
+      <div className='h-4/5  '>
+        <div className='h-5/6 relative p-3 '>
           <Image
             fill
             src={reward.image}
@@ -16,11 +16,13 @@ export const RewardComponent: React.FC<RewardComponentProps> = ({ reward }) => {
           />
         </div>
 
-        <div className='h-1/5'>
+        <div className='h-1/6 flex items-center justify-center'>
           <span className='font-semibold text-sm text-red-500 text-center'>
             {reward.name}
           </span>
         </div>
+
+        <hr className='w-11/12 mx-auto' />
       </div>
 
       <div className='h-1/5 w-full items-center justify-center flex'>
